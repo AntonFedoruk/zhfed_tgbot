cb0873b29388bcce5ed8554e82c0ea489d69a19a
Anton <antobn.fedoruk@gmail.com>
2021-03-31 13:39:17 +0300

    Initial commit.

---
+++ src/main/java/ua/antonfedoruk/zhfed_tgbot/ZhannaFedorukTelegramBot.java
@@ -0,0 +1,43 @@
+package ua.antonfedoruk.zhfed_tgbot;
+
+import lombok.AccessLevel;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+// This class describes our variant of TelegramWebhookBot bean, that we will declare in config files
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ZhannaFedorukTelegramBot extends TelegramWebhookBot {
+    String webHookPath;
+    String botUserName;
+    String botToken;
+
+    public ZhannaFedorukTelegramBot(DefaultBotOptions options) {
+        super(options);
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUserName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
+        return new SendMessage(Long.toString(update.getMessage().getChatId()), "Initial: "+update.getMessage().getText());
+    }
+
+    @Override
+    public String getBotPath() {
+        return webHookPath;
+    }
+}
---
+++ src/test/java/ua/antonfedoruk/zhfed_tgbot/ZhfedTgbotApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.antonfedoruk.zhfed_tgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhfedTgbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
