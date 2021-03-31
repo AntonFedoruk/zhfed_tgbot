1b1895790bec56ab4229675032b72c63fd7868bc
Anton <antobn.fedoruk@gmail.com>
2021-03-31 13:36:48 +0300

    Initial commit.

---
+++ src/main/java/ua/antonfedoruk/zhfed_tgbot/controller/WebHookController.java
@@ -0,0 +1,25 @@
+package ua.antonfedoruk.zhfed_tgbot.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import ua.antonfedoruk.zhfed_tgbot.ZhannaFedorukTelegramBot;
+
+@RestController
+public class WebHookController {
+    private final ZhannaFedorukTelegramBot telegramBot;
+
+    public WebHookController(ZhannaFedorukTelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    // This method listens all data that can be sent by users and responds accordingly.
+    @PostMapping(value = "/")
+    public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
+        return telegramBot.onWebhookUpdateReceived(update);
+    }
+
+}
